a695bc0340b3c2c05badc4c9436aecbb566d648d
Louai Bouzaher <58881632+louaibouzaher@users.noreply.github.com>
2020-12-20 00:21:42 +0100

    Added Resume Btn

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,7 +6,8 @@ const Hero = () => {
     <div className="flex flex-row flex-wrap sm:flex-nowrap justify-around items-center container h-screen">
       <div className="order-2 sm:order-1 flex-2 flex flex-col justify-center items-start p-10 mx-10 mt-0 sm:mt-10">
         <div className="font-sfb text-4xl m-1 text-white text-left ">
-          Hey there, <br/> I'm <span className='gradientText'> Louai Bouzaher! </span>
+          Hey there, <br /> I'm{" "}
+          <span className="gradientText"> Louai Bouzaher! </span>
         </div>
         <div className="font-sans text-xl font-bold text-left m-1 text-white tracking-wide">
           Computer Science Student, UI/UX Enthusiast and Web Developer. <br />{" "}
@@ -15,13 +16,22 @@ const Hero = () => {
         <div className="container my-3">
           <SocialMedia />
         </div>
-        <button className="hidden sm:block gradientBorder font-sans text-xl border-gray-900  border-solid my-5 mx-3 shadow-2xl rounded-lg font-bold tracking-wide h-12 w-40 align-middle bg-gray-700 bg-opacity-5">Contact Me</button>
+        <div className="flex flex-row">
+          <button className="hidden sm:block gradientBorder font-sans text-xl border-gray-900  border-solid mx-3 shadow-2xl rounded-lg font-bold tracking-wide h-12 w-40 align-middle bg-gray-700 bg-opacity-5">
+            Contact Me
+          </button>
+          <button className="hidden sm:block gradientBorderResume font-sans text-xl border-gray-900  border-solid  mx-3 shadow-2xl rounded-lg font-bold tracking-wide h-12 w-40 align-middle">
+            Resume
+          </button>
+        </div>
       </div>
-      <img src={PersonalPicture} alt='louaiPic' className="mt-10 sm:mt-0 h-1/4 sm:h-2/3 order-1 sm:order-2 shadow-2xl  xl:block rounded-full lg:mr-10 image" />
-    
+      <img
+        src={PersonalPicture}
+        alt="louaiPic"
+        className="mt-10 sm:mt-0 h-1/4 sm:h-2/3 order-1 sm:order-2 shadow-2xl  xl:block rounded-full lg:mr-10 image"
+      />
     </div>
   );
 };
 
 export default Hero;
- 
