339126277e6868734a07b01849c22b6e2ded0a29
Louai Bouzaher <58881632+louaibouzaher@users.noreply.github.com>
2020-12-19 04:03:40 +0100

    Created Contributions

--- src/Components/Conributions.js
+++ src/Components/Conributions.js
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import Contribution from "./Contribution";
-
-const Conributions = () => {
+const Contributions = () => {
+  const [Contributions, setContributions] = useState([
+    {
+      title: "CSE-Inventory-API",
+      description:
+        "REST API Built with Express Js and MongoDb for CSE Inventory Mobile App.",
+      isGithub: true,
+      linkGithub: "https://github.com/louaibouzaher/CSE-Inventory-API",
+      isBehance: false,
+      linkBehance: null,
+      isXd: false,
+      linkXd: null,
+    },
+    {
+      title: "CSE-Inventory-API",
+      description:
+        "REST API Built with Express Js and MongoDb for CSE Inventory Mobile App.",
+      isGithub: true,
+      linkGithub: "https://github.com/louaibouzaher/CSE-Inventory-API",
+      isBehance: false,
+      linkBehance: null,
+      isXd: false,
+      linkXd: null,
+    },
+    {
+      title: "CSE-Inventory-API",
+      description:
+        "REST API Built with Express Js and MongoDb for CSE Inventory Mobile App.",
+      isGithub: true,
+      linkGithub: "https://github.com/louaibouzaher/CSE-Inventory-API",
+      isBehance: false,
+      linkBehance: null,
+      isXd: false,
+      linkXd: null,
+    },
+    {
+      title: "CSE-Inventory-API",
+      description:
+        "REST API Built with Express Js and MongoDb for CSE Inventory Mobile App.",
+      isGithub: true,
+      linkGithub: "https://github.com/louaibouzaher/CSE-Inventory-API",
+      isBehance: false,
+      linkBehance: null,
+      isXd: false,
+      linkXd: null,
+    },
+    {
+      title: "Devfest-2020 Website",
+      description: "Devfest 2020 Event Website designed with Adobe XD",
+      isGithub: false,
+      linkGithub: null,
+      isBehance: false,
+      linkBehance: null,
+      isXd: true,
+      linkXd:
+        "https://xd.adobe.com/view/685be0d7-f89f-4b25-b55c-a4fb805edc14-3d08/",
+    },
+    {
+      title: "CSE-Inventory-API",
+      description:
+        "REST API Built with Express Js and MongoDb for CSE Inventory Mobile App.",
+      isGithub: true,
+      linkGithub: "https://github.com/louaibouzaher/CSE-Inventory-API",
+      isBehance: false,
+      linkBehance: null,
+      isXd: false,
+      linkXd: null,
+    },
+    {
+      title: "Devfest-2020 Website",
+      description: "Devfest 2020 Event Website designed with Adobe XD",
+      isGithub: false,
+      linkGithub: null,
+      isBehance: false,
+      linkBehance: null,
+      isXd: true,
+      linkXd:
+        "https://xd.adobe.com/view/685be0d7-f89f-4b25-b55c-a4fb805edc14-3d08/",
+    },
+    {
+      title: "CSE-Inventory-API",
+      description:
+        "REST API Built with Express Js and MongoDb for CSE Inventory Mobile App.",
+      isGithub: true,
+      linkGithub: "https://github.com/louaibouzaher/CSE-Inventory-API",
+      isBehance: false,
+      linkBehance: null,
+      isXd: false,
+      linkXd: null,
+    },
+    {
+      title: "Devfest-2020 Website",
+      description: "Devfest 2020 Event Website designed with Adobe XD",
+      isGithub: false,
+      linkGithub: null,
+      isBehance: false,
+      linkBehance: null,
+      isXd: true,
+      linkXd:
+        "https://xd.adobe.com/view/685be0d7-f89f-4b25-b55c-a4fb805edc14-3d08/",
+    },
+  ]);
   return (
     <div className="my-10 flex flex-col w-full h-screen gradientBackground">
       <div className="self-end m-6 ">
@@ -13,11 +113,15 @@ const Conributions = () => {
           Some code here and there, and design too!
         </span>{" "}
       </div>
-      <div className='flex flex-row'>
-        <Contribution />
+      <div className="flex flex-row overflow-x-scroll">
+        {Contributions.map((c,idx) => {
+          return (
+            <Contribution c={c} key={idx}/>
+          )
+        })}
       </div>
     </div>
   );
 };
 
-export default Conributions;
+export default Contributions;
--- src/Components/Contribution.js
+++ src/Components/Contribution.js
@@ -2,44 +2,47 @@ import React from "react";
 import { AiFillGithub, AiFillBehanceCircle } from "react-icons/ai";
 import { SiAdobexd } from "react-icons/si";
 
-const Contribution = () => {
-  const isGithub = true;
-  const isBehance = true;
-  const isXd = true;
-
+const Contribution = ({c}) => {
+  const {
+    title,
+    description,
+    isGithub,
+    isBehance,
+    isXd,
+    linkGithub,
+    linkXd,
+    linkBehance,
+  } = c
   return (
-    <div className="w-80 h-80 flex flex-col items-center p-5 m-10 rounded-2xl shadow-2xl grayGradientBackground">
+    <div className="flex-grow h-80 flex flex-col items-center p-5 m-10 rounded-2xl shadow-2xl grayGradientBackground">
       <div className="w-4/5 flex-grow-0 p-4 bg-gray-900 rounded-xl shadow-xl">
-        <div className="text-white font-sfb text-xl">Name Of Project</div>
+        <div className="text-white font-sfb text-xl">{title ? title : 'NULL'}</div>
       </div>
       <p className="my-2 text-sm text-white font sfl font-bold text-left tracking-wider ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quis
-        consectetur. Magnam tempora voluptas totam blanditiis. Saepe repellendus
-        enim porro possimus, aliquam illo in iure eius, omnis ipsa repudiandae
-        molestiae.
+       {description}
       </p>
       <div className="flex flex-row justify-around mx-auto">
-        <div className="p-2 mx-1 rounded-full bg-gray-900 flex flex-row justify-around">
           {isGithub && (
+        <div className="p-2 mx-1 rounded-full bg-gray-900 flex flex-row justify-around">
             <AiFillGithub
               className="text-white hover:text-green-400"
               size={35}
             />
-          )}
         </div>
+          )}
+          {isBehance && (
         <div className="p-2 mx-1 rounded-full bg-gray-900 flex flex-row justify-around">
-          {isXd && (
             <AiFillBehanceCircle
               className="text-white hover:text-green-400"
               size={35}
             />
-          )}
         </div>
+          )}
+          {isXd && (
         <div className="p-2 mx-1 rounded-full bg-gray-900 flex flex-row items-center">
-          {isBehance && (
             <SiAdobexd className="text-white hover:text-green-400" size={30} />
-          )}
         </div>
+          )}
       </div>
     </div>
   );
